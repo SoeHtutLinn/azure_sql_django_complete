d5a5487bee21346a0b51fe7a186872336efb4605
soehtutlinn <soehtutlinn.shl@gmail.com>
2026-02-12 17:18:52 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -107,10 +107,10 @@ import urllib.parse
 
 # MongoDB Configuration
 mongo_username = urllib.parse.quote_plus('shl')
-mongo_password = urllib.parse.quote_plus('@dminPassword')
+mongo_password = urllib.parse.quote_plus('adminPassword123')
 # MONGO_URI = 'mongodb://localhost:27017/'
-MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@shl-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
-MONGO_DB_NAME = 'shl-mongodb'
+MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@shl-azure-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
+MONGO_DB_NAME = 'shl-azure-mongodb'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
